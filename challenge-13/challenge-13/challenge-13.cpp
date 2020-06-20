de006e598f7ad9974a20d58425303c5ca1a749c8
James Roberts <j.roberts536@gmail.com>
2020-06-20 15:17:00 +0100

    solve challenge 13

--- challenge-13/challenge-13/challenge-13.cpp
+++ challenge-13/challenge-13/challenge-13.cpp
@@ -17,26 +17,69 @@ std::string profileFor(std::string emailAddress)
 	std::remove(std::begin(emailAddress), std::end(emailAddress), '=');
 	std::remove(std::begin(emailAddress), std::end(emailAddress), '&');
 
-	// Random IDs is sufficient for this challenge
-	auto user = User(emailAddress, std::rand(), "user");
+	// Pseudo-random 3-digit IDs are sufficient for this challenge
+	auto id = std::rand() % 900 + 100;
+	auto user = User(emailAddress, id, "user");
 	std::string serialised = KVSerialise(user.toMap());
 	return serialised;
 }
 }
 
+namespace
+{
+std::size_t blockSize{ 16 };
+}
+
 int main()
 {
-	auto encodedProfile{ CPals::profileFor("myName@myDomain.com") };
-	auto key{ CPals::StringToBuffer("YELLOW SUBMARINE") };
-
+	const auto key{ CPals::StringToBuffer("YELLOW SUBMARINE") };
 	CPals::AesEcbCipher cipher;
-	CPals::Buffer ciphertext{ cipher.Encrypt(CPals::StringToBuffer(encodedProfile), key) };
 
-	CPals::Buffer plaintext{ cipher.Decrypt(ciphertext, key) };
-	std::string plaintextString{ plaintext.cbegin(), plaintext.cend() };
+	/*
+	First we construct a profile whose second 16-byte block will be
+	"admin\x00...\x00" i.e. a padded block starting with "admin". Since
+	ECB encrypts block by block, the second block of the ciphertex will be the
+	encrypted version of this block.
+	*/
+	std::string email{ "xxxxxxxxxxadmin" };
+	for (auto i = 0; i < 11; i++)
+	{
+		email.push_back(0);
+	}
+	email.append("@bar.com");
 
-	std::map<std::string, std::string> profile{ CPals::KVParse(plaintextString) };
-	std::cout << "Decrypted profile:" << std::endl;
+	auto encodedProfile{ CPals::profileFor(email) };
+	auto ciphertext{
+		cipher.Encrypt(CPals::StringToBuffer(encodedProfile), key) };
+
+	CPals::Buffer adminBlock;
+	std::copy_n(ciphertext.cbegin() + blockSize,
+				blockSize,
+				std::back_inserter(adminBlock));
+
+	/*
+	Next we construct a profile which has "user" on its own at the start of
+	its final block. This will be padded to "user\x00...\x00", and we can swap
+	that encrypted block with the encrypted admin block we just made.
+
+	Let's suppose we know the ids have three digits. We could deduce this by
+	submitting progressively longer email addresses until the ciphertext
+	overflows into the next block.
+	*/
+	email = "admin@bar.com";
+	encodedProfile = CPals::profileFor(email);
+	ciphertext = cipher.Encrypt(CPals::StringToBuffer(encodedProfile), key);
+	std::copy_n(adminBlock.begin(), blockSize, ciphertext.end() - blockSize);
+
+	/*
+	Suppose the attacker has sent their modified ciphertext to the server. The
+	server now decrypts the ciphertext and processes the new profile.
+	*/
+	const auto plaintext{ cipher.Decrypt(ciphertext, key) };
+	const std::string plaintextString{ plaintext.cbegin(), plaintext.cend() };
+
+	const auto profile{ CPals::KVParse(plaintextString) };
+	std::cout << "Decrypted profile received by the server:" << std::endl;
 	for (const auto pair : profile)
 	{
 		std::cout << pair.first << " : " << pair.second << std::endl;
