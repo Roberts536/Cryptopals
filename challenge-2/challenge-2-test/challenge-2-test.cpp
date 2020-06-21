474a08eb1bd8f939d59d362ed96aa84f354bed06
James Roberts <j.roberts536@gmail.com>
2020-06-21 18:54:48 +0100

    adapt challenge 2 to repo format

--- challenge-2/challenge-2-test/challenge-2-test.cpp
+++ challenge-2/challenge-2-test/challenge-2-test.cpp
@@ -1,34 +1,34 @@
 #include "gtest/gtest.h"
 #include "HexXor.h"
 
-TEST(TestHexXor, Challenge) {
+TEST(TestHexXor, HexXor_CalledWithChallengeInput_ReturnsCorrectResult) {
 	const std::string s1{ "1c0111001f010100061a024b53535009181c" };
 	const std::string s2{ "686974207468652062756c6c277320657965" };
 	const std::string s3{ "746865206b696420646f6e277420706c6179" };
 	EXPECT_EQ(HexXor(s1, s2), s3);
 }
 
-TEST(TestHexXor, OddLengthHex) {
+TEST(TestHexXor, HexXor_CalledWithOddLengthHex_ReturnsCorrectResult) {
 	const std::string s1{ "abc" };
 	const std::string s2{ "def" };
 	const std::string s3{ "753" };
 	EXPECT_EQ(HexXor(s1, s2), s3);
 }
 
-TEST(TestHexXor, EvenLengthHex) {
+TEST(TestHexXor, HexXor_CalledWithEvenLengthHex_ReturnsCorrectResult) {
 	const std::string s1{ "abc1" };
 	const std::string s2{ "def2" };
 	const std::string s3{ "7533" };
 	EXPECT_EQ(HexXor(s1, s2), s3);
 }
 
-TEST(TestHexXor, EmptyStringInput) {
+TEST(TestHexXor, HexXor_CalledWithEmptyInput_ReturnsCorrectResult) {
 	const std::string s1{ "" };
 	const std::string s2{ "" };
 	const std::string s3{ "" };
 }
 
-TEST(TestHexXor, DifferentStringLengths)
+TEST(TestHexXor, HexXor_CalledDifferentLengthInputs_ThrowsException)
 {
 	const std::string s1{ "ABC" };
 	const std::string s2{ "ABCD" };
